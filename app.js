b45e8e84215a62310e8b78ad02a0a2f96fc2ad96
dumaMandimo <166363285+dumaMandimo@users.noreply.github.com>
2024-04-29 15:54:11 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const staticPath = path.join(__dirname);
+
+// middleware
+
+app.set('view engine', 'ejs');
+app.use(express.static(staticPath));
+
+// routes
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+const PORT = process.env.PROCESS || 3000;
+app.listen(PORT, () => {
+  console.log('port up');
+});
